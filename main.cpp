92542b96145b92cbddec7f562577f0ac99a108f6
25cbarber <25cbarber@go.dsdmail.net>
2025-01-09 15:50:37 +0000

    done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int age;
+    string name;
+
+    cout << "Hello World!!" << endl;
+    cout << "How old are you?: ";
+    cin >> age;
+
+    cin.ignore();
+
+    cout << "What is your full name?: ";
+    getline(cin, name);
+
+    cout << "Welcome " << name << "!" << " You are " << age << " years old!" << endl;
+
+
+
+}
