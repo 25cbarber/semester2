3797c57c9071126adf28608b32ec9883319bbdbe
25cbarber <25cbarber@go.dsdmail.net>
2025-01-09 16:55:58 +0000

    working

---
+++ methods.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int num;
+
+bool isEven() {
+
+    if (num % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
+     }
+
+
+
+int main() {
+    
+    
+}
