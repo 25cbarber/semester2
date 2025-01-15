358f22d2cf4109d43923adec32edd23433baf75b
25cbarber <25cbarber@go.dsdmail.net>
2025-01-15 17:17:20 +0000

    woeking]

--- methods.cpp
+++ methods.cpp
@@ -1,21 +1,83 @@
 #include <iostream>
 using namespace std;
 
-int num;
+int sum = 0;
 
-bool isEven() {
-
-    if (num % 2 == 0) {
-        return true;
-    } else {
-        return false;
-    }
-    
-     }
+bool isEven(int num);
+int sumOfDigits(int num);
+bool isPrime(int num);
+bool isSpecialNumber(int num);
 
 
 
 int main() {
+    int num;
+    cout << "enter a number" << endl;
+    cin >> num;
+    cout << sumOfDigits(num) << endl;
+    cout << isPrime(num) << endl;
+    cout << isSpecialNumber(num) << endl;
+
+
+    return 0;
     
-    
+}
+
+int sumOfDigits(int num)
+{
+    while (num != 0)
+    {
+
+        int last = num % 10;
+
+        sum += last;
+
+        num /= 10;
+    }
+    return sum;
+    return num;
+}
+
+bool isEven(int num)
+{
+
+    if (num % 2 == 0)
+    {
+        cout << "true" << endl;
+        return true;
+    }
+    else
+    {
+        cout << "false" << endl;
+        return false;
+    }
+}
+
+bool isPrime(int num)
+{
+    if (num <= 1)  {
+        return false;
+    }
+
+    int cnt =0;
+    for (int i = 1; i <= num; i++) {if (num % i == 0)  {
+        cnt++;
+        }
+    }
+
+    return (cnt ==2); 
+}
+
+bool isSpecialNumber(int num) {
+    num = sum;
+    if (isEven) {
+
+        return true;
+        cout << "true" << endl;
+    }
+    else {
+        return false;
+        cout << "false" << endl;
+    }
+
 }
