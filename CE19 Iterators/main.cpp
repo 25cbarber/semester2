a05186174c2c72124acac1bbea17931f9be569b0
25cbarber <25cbarber@go.dsdmail.net>
2025-04-22 15:12:00 +0000

    done

--- CE19 Iterators/main.cpp
+++ CE19 Iterators/main.cpp
@@ -16,20 +16,20 @@ int main() {
 
     cout << "All Items:" << endl;
     for (vector<Artifact>::iterator it = items.begin(); it != items.end(); ++it) {
-        cout << *it << endl; // Using the overloaded operator
+        cout << *it << endl; 
     }
 
     vector<Artifact>::iterator most_valuable = max_element(items.begin(), items.end(), [](const Artifact& a, const Artifact& b) {
         return a.value < b.value;
     });
     cout << "\nMost Valuable Item:" << endl;
-    cout << *most_valuable << endl; // Using the overloaded operator
+    cout << *most_valuable << endl;
 
     vector<Artifact>::iterator least_valuable = min_element(items.begin(), items.end(), [](const Artifact& a, const Artifact& b) {
         return a.value < b.value;
     });
     cout << "\nLeast Valuable Item:" << endl;
-    cout << *least_valuable << endl; // Using the overloaded operator
+    cout << *least_valuable << endl;  
 
     return 0;
 }
