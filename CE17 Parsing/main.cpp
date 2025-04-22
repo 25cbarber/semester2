98e31c4f21bfb692d7b9a2b27ab473e89d97e51e
25cbarber <25cbarber@go.dsdmail.net>
2025-04-22 14:09:08 +0000

    done

--- CE17 Parsing/main.cpp
+++ CE17 Parsing/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-
+#include <limits>
+// i prefer the second method because it was easier for me to code. I think the second method would be better for a 
+//large scale project because it is more flexible.
 using namespace std;
 
 void parseByStream() {
@@ -14,20 +15,45 @@ void parseByStream() {
 
     cout << "Enter a delimiter: " << endl;
     cin >> charDelimiter;
-    
-    istringstream iss(stringInput);
-    while (iss, charDelimiter) {
-        cout << stringInput << endl;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string result;
+    for (size_t i = 0; i < stringInput.size(); i++) {
+        if (stringInput[i] == ' ') {
+            result += charDelimiter;
+        } else {
+            result += stringInput[i]; 
+        }
     }
 
+    cout << "Result: " << result << endl;
+}
 
+void parseByStr() {
+
+    string stringInput;
+    string stringDelimiter;
+
+    cout << "Enter a string: " << endl;
+    getline(cin, stringInput);
+
+    cout << "Enter a delimiter: " << endl;
+    cin >> stringDelimiter;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string result;
+    for (size_t i = 0; i < stringInput.size(); i++) {
+        if (stringInput[i] == ' ') {
+            result += stringDelimiter;
+        } else {
+            result += stringInput[i]; 
+        }
+    }
+
+    cout << "Result: " << result << endl;
 }
 
 int main() {
-
-    parseByStream();
-
-
-
-
+    parseByStr();
+    return 0;
 }
